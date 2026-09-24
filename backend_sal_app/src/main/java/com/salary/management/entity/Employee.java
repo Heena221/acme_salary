652ecf9fb10abedd59e606d124871d2d186effdb
S Heena Fariyad <115779620+Heena221@users.noreply.github.com>
2026-09-24 15:45:43 +0530

    Add files via upload
    initial backend commit

---
+++ backend_sal_app/src/main/java/com/salary/management/entity/Employee.java
@@ -0,0 +1,85 @@
+package com.salary.management.entity;
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    private String name;
+
+    private String country;
+
+    private String department;
+
+    private Double salary;
+
+    private String currency;
+
+    public Employee() {
+    }
+
+    public Employee(Long employeeId, String name, String country,
+                    String department, Double salary, String currency) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.country = country;
+        this.department = department;
+        this.salary = salary;
+        this.currency = currency;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
